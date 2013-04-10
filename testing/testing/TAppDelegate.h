f727f41b660db8e523fe44bc61ede20045f1e3b2
karencatearabit <karen@klab.com>
2013-04-10 16:53:55 +0800

    deleted project folder

---
+++ testing/testing/TAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TAppDelegate.h
+//  testing
+//
+//  Created by Karen Cate Arabit on 4/10/13.
+//  Copyright (c) 2013 Karen Cate Arabit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ testing/testing/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  testing
+//
+//  Created by Karen Cate Arabit on 4/10/13.
+//  Copyright (c) 2013 Karen Cate Arabit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TAppDelegate class]));
+    }
+}
